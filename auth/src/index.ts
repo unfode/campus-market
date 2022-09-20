cfda5b88f689008bf7251a385762633bec7ef07f
unfode <forrest.futao.wei@gmail.com>
2022-09-20 00:36:51 -0400

    Add error-handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
 import { usersRouter } from './routes/users';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
