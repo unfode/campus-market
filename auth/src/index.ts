76d47aa79150f4a048299272b557dc038e2feca4
unfode <forrest.futao.wei@gmail.com>
2022-10-16 21:28:44 -0400

    Add not-found-error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 import { usersRouter } from './routes/users';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
+
+app.use('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
