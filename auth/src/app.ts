df460f26e253a18a523e05b90e71049858df7d16
unfode <forrest.futao.wei@gmail.com>
2022-10-21 21:44:13 -0400

    Refactor code for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from "cookie-session";
+
+import { usersRouter } from './routes/users';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
+
+app.use('/api/users', usersRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from "cookie-session";
 
-import { usersRouter } from './routes/users';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}));
-
-app.use('/api/users', usersRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import {app} from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
