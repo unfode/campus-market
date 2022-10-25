0e7cd26302391a21d2d48bb6dcf5978df5edf945
unfode <forrest.futao.wei@gmail.com>
2022-10-24 21:08:29 -0400

    Add unit testing on auth

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -11,7 +11,7 @@ app.set('trust proxy', true);
 app.use(express.json());
 app.use(cookieSession({
   signed: false,
-  secure: true
+  secure: process.env.NODE_ENV !== 'test'
 }));
 
 app.use('/api/users', usersRouter);
